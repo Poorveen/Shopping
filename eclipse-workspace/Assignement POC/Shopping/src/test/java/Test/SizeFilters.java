519ac6c0c8476f66554a7477821de428f9374605
poorveen <poorvpatel@gmail.com>
2020-05-21 19:06:09 +0530

    Test Commit

--- eclipse-workspace/Assignement POC/Shopping/src/test/java/Test/SizeFilters.java
+++ eclipse-workspace/Assignement POC/Shopping/src/test/java/Test/SizeFilters.java
@@ -1,14 +1,11 @@
 package Test;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import pageObjects.Homepage;
